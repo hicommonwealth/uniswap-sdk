ebe9ab7ecdac8eae7bb81ae3d8a855ab741aab56
Jake Naviasky <jake@commonwealth.im>
2020-08-19 12:16:15 -0400

    Update constants.

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,7 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
+  EDGEWARE = 7,
   KOVAN = 42
 }
 
@@ -22,7 +23,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xC2Bf5F29a4384b1aB0C063e1c666f02121B6084a'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
