be2e20998df15daf66474408c1756f8f3a61a2f6
Jake Naviasky <jake@commonwealth.im>
2020-08-19 16:11:21 -0400

    Update constants for edgeware.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0xF8cef78E923919054037a1D03662bBD884fF4edf'
 
 // a list of tokens by chain
 type ChainTokenList = {
